a3a68b44737cf787ea1055b7e5e43e491910afd1
Amanbek Nurbek <nurbek_aman@mail.ru>
2022-03-27 20:51:30 +0600

    Cделал задачи по 5 урок

--- task04.js
+++ task04.js
@@ -1,30 +1,18 @@
 'use strict'
 
-const getCountDiscount = (a , b) =>{
-    if (a > 10){
-    b = b * 0.03;
-    return b;
+const getCountDiscount = (countGoods , totalSumGoods) =>{
+    if (countGoods > 10){
+        totalSumGoods = totalSumGoods * 0.03;
+    return totalSumGoods;
     }
 }
 
-const getTotalSimDiscount = (a) =>{
-    let tSum = a;
-if (tSum > 30000){
-    let tSum2 = tSum - 30000;
-    let totalDiscountSum =  tSum2 * 0.15;
+const getTotalSumDiscount = (totalSumGoods) =>{
+if (totalSumGoods > 30000){
+    let totalSumBalance = totalSumGoods - 30000;
+    let totalDiscountSum =  totalSumBalance * 0.15;
     return totalDiscountSum;
-}
-}
-
-const getCodeDiscount = (code, tSum) =>{
-if(code === 'METHOD'){
- tSum = tSum * 0.1;
- return tSum
-}if(code === 'G3H2Z1' && tSum > 2000){
- tSum = tSum - 500;
- return tSum;
-
-}
+    }
 }
 
 const calculate = (totalSum, countGoods, wordDiscount) =>{
@@ -33,29 +21,22 @@ const calculate = (totalSum, countGoods, wordDiscount) =>{
 
     console.log('1 скидка, 3% на кол-во товаров больше 10:', countDiscount);
 
-    const countDiscount15 = getTotalSimDiscount(totalSum);
+    const countDiscount15 = getTotalSumDiscount(totalSum);
 
     console.log('2 скидка, 15% на ост. сумму от 30000:', countDiscount15);
-
-    const countDiscount10 = getCodeDiscount(wordDiscount,totalSum);
-
-    
 
     let balanceSum = totalSum - countDiscount - countDiscount15;
 
     if(wordDiscount === 'METHOD'){
-        totalSum =  totalSum - (totalSum * 0.1) - countDiscount - countDiscount15;
-        console.log('3 скидка, 10% на Код Слово METHOD ', totalSum);
-        return totalSum;
+       let countDiscount10 =  totalSum * 0.1;
+       let totalDiscountSum = totalSum - countDiscount - countDiscount15 - countDiscount10;
+        console.log('3 скидка, 10% на Код Слово METHOD: ', countDiscount10);
+        return totalDiscountSum;
     }if(wordDiscount === 'G3H2Z1' && balanceSum > 2000){
-        balanceSum = balanceSum - 500;
-        console.log('3 скидка, 10% на Код Слово G3H2Z1 ', balanceSum);
-        return balanceSum;
+        let totalDiscountSum = balanceSum - 500;
+        console.log('3 скидка, 10% на Код Слово G3H2Z1: 500');
+        return totalDiscountSum;
     }
-
-
 }
 
-
-
-console.log(calculate(35000, 11, 'METHOD'));
+console.log('Ваша итоговая сумма с учетом скидок: ', calculate(35000, 11, 'G3H2Z1'));
